60bfe7a489b8452c784b05d1d46b5bb90a471888
usiiasirrush <28960829+usiiasirrush@users.noreply.github.com>
2023-04-23 17:18:43 -0400

    Add files via upload

---
+++ src/test/java/io/springtraining/firstsessionspringexample/FirstSessionSpringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springtraining.firstsessionspringexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSessionSpringExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
