128088a7e9611314aca4223b264eb50598d9409d
usiiasirrush <28960829+usiiasirrush@users.noreply.github.com>
2023-05-05 17:20:56 -0400

    Update
    Update code to most recent local version

--- src/main/java/io/springtraining/firstsessionspringexample/FirstSessionSpringExampleApplication.java
+++ src/main/java/io/springtraining/firstsessionspringexample/FirstSessionSpringExampleApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FirstSessionSpringExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FirstSessionSpringExampleApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(FirstSessionSpringExampleApplication.class, args);
+  }
 }
